4c5789c4b17609feccf648d5174cf6b8cec4ac48
KashviAnand <kashvianandrockx@gmail.com>
2026-05-09 16:04:04 +0530

    renamed

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ app.register_blueprint(student_bp)
 with app.app_context():
     db.create_all()
 
-@app.route('/')
-def home():
-    return render_template("index.html")
+@app.route('/test')
+def test():
+    return render_template("add_student.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
